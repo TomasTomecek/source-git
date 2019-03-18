e61d65cba40a85ab02c85cbee332fb1ba72f27e1
Tomas Tomecek <ttomecek@redhat.com>
2019-03-18 15:38:11 +0100

    fix CLI of sync-from-downstream
    Signed-off-by: Tomas Tomecek <ttomecek@redhat.com>

--- packit/cli/sync_from_downstream.py
+++ packit/cli/sync_from_downstream.py
@@ -9,41 +9,33 @@ import click
 
 from packit.api import PackitAPI
 from packit.cli.types import LocalProjectParameter
-from packit.cli.utils import cover_packit_exception
+from packit.cli.utils import cover_packit_exception, get_packit_api
 from packit.config import pass_config, get_context_settings, get_local_package_config
 
 logger = logging.getLogger(__file__)
 
 
-@click.command("sync-upstream", context_settings=get_context_settings())
+@click.command("sync-from-downstream", context_settings=get_context_settings())
 @click.option(
     "--dist-git-branch", help="Source branch in dist-git for sync.", default="master"
-)
-@click.option(
-    "--dist-git-path",
-    help="Path to dist-git repo to work in. "
-    "Otherwise clone the repo in a temporary directory.",
 )
 @click.option(
     "--upstream-branch", help="Target branch in upstream to sync to.", default="master"
 )
 @click.option("--no-pr", is_flag=True, help="Pull request is not create.")
 @click.argument(
-    "repo", type=LocalProjectParameter(), default=os.path.abspath(os.path.curdir)
+    "path_or_url", type=LocalProjectParameter(), default=os.path.abspath(os.path.curdir)
 )
 @pass_config
 @cover_packit_exception
 def sync_from_downstream(
-    config, dist_git_path, dist_git_branch, upstream_branch, no_pr, repo
+    config, dist_git_branch, upstream_branch, no_pr, path_or_url
 ):
     """
-    Sync downstream release into upstream
+    Copy synced files from Fedora dist-git into upstream by opening a pull request.
 
-    REPO argument is a local path to the upstream git repository,
+    PATH_OR_URL argument is a local path or a URL to the upstream git repository,
     it defaults to the current working directory
     """
-    package_config = get_local_package_config(directory=repo.working_dir)
-    package_config.downstream_project_url = dist_git_path
-    package_config.upstream_project_url = repo.working_dir
-    api = PackitAPI(config=config, package_config=package_config)
+    api = get_packit_api(config=config, local_project=path_or_url)
     api.sync_from_downstream(dist_git_branch, upstream_branch, no_pr)
